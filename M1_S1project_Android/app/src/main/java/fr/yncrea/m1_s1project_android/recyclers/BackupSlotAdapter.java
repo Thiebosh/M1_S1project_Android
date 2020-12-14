207042036257a03f2650f3ec04d83ac3f3c68da6
JarJarBinksIsen <36074939+JarJarBinksIsen@users.noreply.github.com>
2020-12-14 14:51:50 +0100

    background slot when onclick

--- M1_S1project_Android/app/src/main/java/fr/yncrea/m1_s1project_android/recyclers/BackupSlotAdapter.java
+++ M1_S1project_Android/app/src/main/java/fr/yncrea/m1_s1project_android/recyclers/BackupSlotAdapter.java
@@ -1,5 +1,6 @@
 package fr.yncrea.m1_s1project_android.recyclers;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,30 +20,55 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
     public static class Holder extends RecyclerView.ViewHolder {
 
         private final TextView mSlotContainer;
+        public int mSlotSelected;
 
         public Holder(@NonNull View itemView) {
             super(itemView);
             mSlotContainer = itemView.findViewById(R.id.slot_textview);
+            mSlotSelected = -1;
+        }
+
+        public TextView getSlotContainer(){
+            return mSlotContainer;
         }
 
         public void setDisplay(final int position) {
             mSlotContainer.setText(itemView.getContext().getString(R.string.slot, position));
+            mSlotContainer.setTag(position);
         }
 
-        public void setInteractions(BackupConfigAdapter displayer, final ArrayList<Channel> config) {
+        public void setInteractions(BackupSlotAdapter adapter, BackupConfigAdapter displayer, final ArrayList<Channel> config) {
             mSlotContainer.setOnClickListener(v -> {
                 //apply color on item
-                displayer.getChannelList().clear();
-                displayer.getChannelList().addAll(config);
-                displayer.notifyDataSetChanged();
-                
-                //mSlotContainer.setBackgroundColor(itemView.getContext().getResources().getColor(R.color.yellow));
+                //displayer.getChannelList().clear();
+                //displayer.getChannelList().addAll(config);
+                //displayer.notifyDataSetChanged();
+                if (this != adapter.getLastHolderSelected()) {//pour empecher deselection de digit
+                    if (adapter.getLastHolderSelected() != null) {
+                        adapter.getLastHolderSelected().getSlotContainer().setBackgroundColor(itemView.getContext().getResources().getColor(R.color.gray));
+                    }
+
+                    mSlotContainer.setBackgroundColor(itemView.getContext().getResources().getColor(R.color.yellow));
+
+                    adapter.setLastHolderSelected(this);
+                }
             });
         }
+
     }
 
     private final ArrayList<Generator> mConfigList;
     private final BackupConfigAdapter mConfigDisplayer;
+    private BackupSlotAdapter.Holder mLastHolderSelected = null;
+
+    public Holder getLastHolderSelected() {
+        return mLastHolderSelected;
+    }
+
+    public void setLastHolderSelected(Holder holder){
+        this.mLastHolderSelected = holder;
+    }
+
 
     public BackupSlotAdapter(BackupConfigAdapter configDisplayer, ArrayList<Generator> configList) {
         this.mConfigDisplayer = configDisplayer;
@@ -59,8 +85,10 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.setDisplay(position);
-        holder.setInteractions(mConfigDisplayer, mConfigList.get(position).getChannelList());
+        holder.setInteractions(this, mConfigDisplayer, mConfigList.get(position).getChannelList());
         //((TextView)holder.itemView).setText(holder.itemView.getContext().getString(R.string.slot, position));
+
+
     }
 
     @Override
