5cd9a32cdf05f939cadcb504f2daeab360f6bd24
JarJarBinksIsen <36074939+JarJarBinksIsen@users.noreply.github.com>
2020-12-14 17:32:14 +0100

    Communication bluetooth du backup

--- M1_S1project_Android/app/src/main/java/fr/yncrea/m1_s1project_android/recyclers/BackupSlotAdapter.java
+++ M1_S1project_Android/app/src/main/java/fr/yncrea/m1_s1project_android/recyclers/BackupSlotAdapter.java
@@ -1,5 +1,8 @@
 package fr.yncrea.m1_s1project_android.recyclers;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +24,13 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
     public static class Holder extends RecyclerView.ViewHolder {
 
         private final TextView mSlotContainer;
-        public int mSlotSelected;
+        public Context ctx;
+
 
         public Holder(@NonNull View itemView) {
             super(itemView);
             mSlotContainer = itemView.findViewById(R.id.slot_textview);
-            mSlotSelected = -1;
+            ctx = itemView.getContext();
         }
 
         public TextView getSlotContainer(){
@@ -38,8 +42,9 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
             mSlotContainer.setTag(position);
         }
 
-        public void setInteractions(BackupSlotAdapter adapter, BackupConfigAdapter displayer, final ArrayList<Channel> config) {
+        public void setInteractions(BackupSlotAdapter adapter, BackupConfigAdapter displayer, final ArrayList<Channel> config, Context context) {
             mSlotContainer.setOnClickListener(v -> {
+                ((BluetoothParent) getActivity(context)).sendData("slot"+getAdapterPosition());
                 //apply color on item
                 //displayer.getChannelList().clear();
                 //displayer.getChannelList().addAll(config);
@@ -53,7 +58,6 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
 
                     adapter.setLastHolderSelected(this);
 
-                    ((BluetoothParent) itemView.getContext()).sendData("slot"+getAdapterPosition());
                 }
             });
         }
@@ -88,7 +92,7 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.setDisplay(position);
-        holder.setInteractions(this, mConfigDisplayer, mConfigList.get(position).getChannelList());
+        holder.setInteractions(this, mConfigDisplayer, mConfigList.get(position).getChannelList(), holder.itemView.getContext());
         //((TextView)holder.itemView).setText(holder.itemView.getContext().getString(R.string.slot, position));
 
 
@@ -101,5 +105,15 @@ public class BackupSlotAdapter extends RecyclerView.Adapter<BackupSlotAdapter.Ho
 
     public int getFocusedIndex() {
         return 1;
+    }
+
+    private static Activity getActivity(Context context) {
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
     }
 }
